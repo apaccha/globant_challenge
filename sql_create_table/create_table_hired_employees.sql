ccc2514ce09e2fedee2b28ef2d9a217caad472dc
Alejandro Paccha <apaccha@go2clouds.com>
2024-06-05 22:35:53 -0500

    source files

---
+++ sql_create_table/create_table_hired_employees.sql
@@ -0,0 +1,7 @@
+create table hired_employees (
+id integer,
+name varchar(255),
+datetime datetime,
+department_id integer,
+job_id integer
+)
---
+++ sql_create_table/create_table_jobs.sql
@@ -0,0 +1,4 @@
+create table jobs(
+id integer,
+job varchar(255)
+)
